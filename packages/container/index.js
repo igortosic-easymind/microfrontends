ccee340fbb31788c8f4807847fe997469bf8d7b7
igortosic-easymind <igortosic1980@gmail.com>
2020-12-28 21:13:26 +0100

    deleted redundant files and updated package.json

--- packages/container/index.js
+++
@@ -1,5 +0,0 @@
-import('./src/bootstrap');
-/**
-This just gives Wabpack when it's running inside the browser a little bit of time to make sure that
-we can load up all the appropriate dependencies to actually run our bootstrapped JS file.
- */
