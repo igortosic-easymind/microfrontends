0055ca10805a84385d74d31aba0ea7aa67e43b82
igortosic-easymind <igortosic1980@gmail.com>
2020-12-28 20:56:45 +0100

    trying to fix version in package.json

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,25 +1,26 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+//import { useHistory } from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
-    const history = useHistory();
+    //const history = useHistory();
 
     useEffect(() => {
-        const { onParentNavigate } = mount(ref.current, {
-            initialPath: history.location.pathname,
-            onNavigate: ({ pathname: nextPathname }) => {
-                const { pathname } = history.location;
+        //const { onParentNavigate } = mount(ref.current, {
+        //initialPath: history.location.pathname,
+        //onNavigate: ({ pathname: nextPathname }) => {
+        //const { pathname } = history.location;
 
-                if (pathname !== nextPathname) {
-                    history.push(nextPathname);
-                }
-            },
-        });
+        //if (pathname !== nextPathname) {
+        //history.push(nextPathname);
+        mount(ref.current);
+        //}
+        //},
+    });
 
-        history.listen(onParentNavigate);
-    }, []);
+    //history.listen(onParentNavigate);
+    //}, []);
 
-    return <div ref={ref} />;
-};
+    return <div ref={ref} />
+}
